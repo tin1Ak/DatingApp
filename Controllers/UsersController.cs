f996887f26ae8df11ca3c8aa02643fe40a8e82ad
Thinhinane AKKOUL <akkoultina@gmail.com>
2021-06-02 15:47:13 +0200

    Init Project

---
+++ Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using DatingApp.Data;
+using DatingApp.Entities;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DatingApp.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly DataContext _context;
+
+        public UsersController(DataContext context)
+        {
+           _context = context;
+        }
+
+        [HttpGet]
+        //IEnumerable allows simple iteration to the the collection
+        public ActionResult<IEnumerable<AppUser>> GetUsers()
+        {
+            var users = _context.Users.ToList();
+            return users;
+        }
+        //api/users/3(id)
+        [HttpGet("{id}")]
+        public ActionResult<AppUser> GetUser(int id)
+        {
+            return _context.Users.Find(id);
+        }
+    }
+}
